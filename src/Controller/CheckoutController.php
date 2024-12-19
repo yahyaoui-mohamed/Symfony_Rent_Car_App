7aa7be94a438d9316a52695ab25fad7e06b1a92c
yahyaoui-mohamed <hamayah4@gmail.com>
2024-12-19 15:31:45 +0100

    Add login

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Car;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CheckoutController extends AbstractController
 {
-    #[Route('/checkout', name: 'app_checkout')]
-    public function index(): Response
+    #[Route('/checkout/{id}', name: 'app_checkout')]
+    public function index($id, EntityManagerInterface $em): Response
     {
-        return $this->render('checkout/index.html.twig', []);
+        $car = $em->getRepository(Car::class)->find($id);
+        return $this->render('checkout/index.html.twig', [
+            'car' => $car
+        ]);
     }
 }
---
+++ src/Controller/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->render('login/login.html.twig');
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
