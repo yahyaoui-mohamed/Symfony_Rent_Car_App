fac328c900d8fe7504d7e491c46cbc291b6be7d9
yahyaoui-mohamed <hamayah4@gmail.com>
2024-12-20 02:28:58 +0100

    Add user datafixture

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -2,16 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixture extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setEmail("user" . $i + 1 . "@gmail.com");
+            $password = $this->hasher->hashPassword($user, $i + 1 . "abc");
+            $user->setPassword($password);
+            $manager->persist($user);
+        }
         $manager->flush();
     }
 }
