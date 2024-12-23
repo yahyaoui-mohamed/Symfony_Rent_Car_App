fbfb9afeecfbb8468ae5afb95b2cf662fb62b7dc
yahyaoui-mohamed <hamayah4@gmail.com>
2024-12-23 20:10:55 +0100

    Fix admin dashboard

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,7 +12,7 @@ class AccountController extends AbstractController
     #[Route('/account', name: 'app_account')]
     public function index(): Response
     {
-        
+
         return $this->render('account/index.html.twig');
     }
 }
