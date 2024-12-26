594c8ecd5fe41ff5f646368a0bf3b28b46d98c69
yahyaoui-mohamed <hamayah4@gmail.com>
2024-12-26 14:26:52 +0100

    Fix some bigs

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -2,15 +2,54 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SettingsController extends AbstractController
 {
-    #[Route('/admin/settings', name: 'app_settings')]
-    public function index(): Response
+    #[Route('/settings', name: 'app_settings')]
+    public function index(Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $em): Response
     {
-        return $this->render('settings/index.html.twig');
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+        $form = $this->createFormBuilder()
+            ->add("email", EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Email'
+                ]
+            ])
+            ->add("password", PasswordType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Password'
+                ]
+            ])
+            ->add("save", SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();
+            $data = $form->getData();
+            $user->setEmail($data["email"]);
+            $HashedPassword = $hasher->hashPassword($user, $data["password"]);
+            $user->setPassword($HashedPassword);
+            $em->flush();
+            return $this->redirectToRoute('app_settings');
+        }
+
+        return $this->render('settings/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
