76374427e66e1058bc9eb85efd9e9b4a63c63694
yahyaoui-mohamed <hamayah4@gmail.com>
2024-12-30 19:25:46 +0100

    General fixes

---
+++ src/Controller/NotificationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class NotificationController extends AbstractController
+{
+    #[Route('/notification', name: 'app_notification')]
+    public function index(): Response
+    {
+        return $this->render('notification/index.html.twig');
+    }
+}
