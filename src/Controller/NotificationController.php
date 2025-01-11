005e3457328d7ecbd83b5a4101c6a6e4518472f4
yahyaoui-mohamed <hamayah4@gmail.com>
2025-01-11 15:14:05 +0100

    Fix some bugs

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 class NotificationController extends AbstractController
 {
     #[Route('/notification', name: 'app_notification')]
-    public function index(): Response
+    public function index(): JsonResponse
     {
+
         return $this->render('notification/index.html.twig');
     }
 }
