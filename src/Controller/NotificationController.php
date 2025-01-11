3578c6b26100c1f5da26aef7b853e26982aafcff
yahyaoui-mohamed <hamayah4@gmail.com>
2025-01-11 15:18:53 +0100

    Fix vugs

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -3,13 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NotificationController extends AbstractController
 {
     #[Route('/notification', name: 'app_notification')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
 
         return $this->render('notification/index.html.twig');
